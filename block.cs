b0e4bcabd850ea4146189d0d96ff9d78d6454877
shanielgamil <elgamilshani@gmail.com>
2023-07-25 19:47:16 +0300

    sss
    j

--- block.cs
+++ block.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing;
 namespace funkyBubbles
 {
-    class block
+    class Block : Entity
     {
+
+        Image number1;
+         public  Block(float x, float y, int size) : base(x, y, size)
+        {
+            this.radius = size;
+            b = new SolidBrush(Color.Red);
+
+            number1= Image.FromFile("pics\\block.png");
+
+        }
+
+        public abstract void draw(Graphics g)
+        {
+            g.DrawImage(block, x -200, y +500, 150, 100);
+        }
+        //hgfdfd
     }
 }
